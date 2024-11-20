1ee8305049837ccb2dec67b3e7586e07b27b31c3
Alan Chavarin <alanchavarin4@hotmail.com>
2024-11-19 17:51:01 -0800

    fixed background issues

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} antialiased flex flex-col min-h-screen`}> 
+      <body className={`${geistSans.variable} antialiased flex flex-col min-h-screen bg-white`}> 
         <Navbar />
         {children}
         <Footer />
